5c40b539a0e70131d5871f8e861d6113a23440d0
István Janó <janoist1@gmail.com>
2019-10-12 15:26:49 +0100

    Updates

---
+++ precache-manifest.026902fe4bba2085106590200f09e8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9988eefae64eb47adb040df9991360",
+    "url": "/wordybirdy/index.html"
+  },
+  {
+    "revision": "37fb728f661a06478272",
+    "url": "/wordybirdy/static/css/main.88918f04.chunk.css"
+  },
+  {
+    "revision": "3f7e709597b96ed22097",
+    "url": "/wordybirdy/static/js/2.6a63b45e.chunk.js"
+  },
+  {
+    "revision": "37fb728f661a06478272",
+    "url": "/wordybirdy/static/js/main.a8c2677d.chunk.js"
+  },
+  {
+    "revision": "488f79da9499b533ef1e",
+    "url": "/wordybirdy/static/js/runtime-main.eb7d1235.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/wordybirdy/static/media/logo.25bf045c.svg"
+  }
+]);
